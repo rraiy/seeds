d4c41f6d8695d8efae54b395a06a4c8a1484fdd5
rraiy <raiybow@gmail.com>
2024-06-06 14:24:39 +0800

    ui: update about text

--- app/components/common/TextWithCards.tsx
+++ app/components/common/TextWithCards.tsx
@@ -2,6 +2,8 @@ import { Grid, GridItem } from '@chakra-ui/react';
 
 import MultiCards from '@/app/components/common/MultiCards';
 
+import styles from '@/app/styles/components/common/TextWithCards.module.scss';
+
 const mock = [
   {
     imgSrc: '/image/about/info.png',
@@ -41,13 +43,13 @@ const mock = [
 const TextWithCards = () => {
   return (
     <Grid templateRows="256px 440px">
-      <GridItem>
+      <GridItem className={styles['text-container']}>
         <h2>關於喜茲</h2>
         <p>
           我們是喜茲體能 Seeds Training
           不同於一般健身房或者重訓課程的強烈印象，喜茲偏向以「適合全年齡的肌力訓練」提供專業的課程規劃。並且與越野跑協會合作，開發越野跑體能訓練課程。
-          基本上，喜茲不單只是教動作，課程中會給予適當的體能基礎知識去結合實作，讓學員更認識自己的身體並學會運用。
         </p>
+        <p>基本上，喜茲不單只是教動作，課程中會給予適當的體能基礎知識去結合實作，讓學員更認識自己的身體並學會運用。</p>
       </GridItem>
       <GridItem>
         <MultiCards cardsContent={mock} />
