30be46f5cf6e876490f0681ecf6a40afd42d5d22
rraiy <raiybow@gmail.com>
2024-07-16 09:25:22 +0800

    optm: banner load

--- app/components/home/HomeBanner.tsx
+++ app/components/home/HomeBanner.tsx
@@ -1,23 +1,62 @@
+'use client';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 import BANNER_IMG from '@/app/constants/content/home/banner';
 import Carousel from '@/app/components/common/carousel/Carousel';
 
 function HomeBanner() {
+  const [imagesLoaded, setImagesLoaded] = useState(Array(BANNER_IMG.length).fill(false));
+  const [allImagesLoaded, setAllImagesLoaded] = useState(false);
+
+  useEffect(() => {
+    if (imagesLoaded.every(Boolean) && !allImagesLoaded) {
+      setAllImagesLoaded(true);
+    }
+  }, [imagesLoaded]);
+
+  const handleImageLoad = (index: number) => {
+    setImagesLoaded((prevState) => {
+      const newState = [...prevState];
+      newState[index] = true;
+      return newState;
+    });
+  };
+
   return (
-    <Carousel className="mobile:min-h-[100%]">
-      {BANNER_IMG.map((img: string) => (
-        <Image
-          key={img}
-          width="0"
-          height="0"
-          sizes="100vw"
-          className="w-full h-auto object-cover mobile:h-[50vh] mobile:object-contain"
-          src={img}
-          alt="喜茲體能"
-        />
-      ))}
-    </Carousel>
+    <div>
+      {allImagesLoaded ? (
+        <Carousel className="mobile:min-h-[100%]">
+          {BANNER_IMG.map((img, index) => (
+            <Image
+              key={img}
+              width="0"
+              height="0"
+              sizes="100vw"
+              className="w-full h-auto object-cover mobile:h-[50vh] mobile:object-contain"
+              src={img}
+              alt="喜茲體能"
+              onLoad={() => handleImageLoad(index)}
+            />
+          ))}
+        </Carousel>
+      ) : (
+        <div>
+          {BANNER_IMG.map((img, index) => (
+            <Image
+              key={img}
+              width="0"
+              height="0"
+              sizes="100vw"
+              // className="hidden"
+              src={img}
+              alt="Loading"
+              onLoad={() => handleImageLoad(index)}
+            />
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
 
